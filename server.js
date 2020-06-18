a1051e8487e6144beaa86669bb4b0ae77858bbb4
sharjeel <torasx3600@gmail.com>
2020-06-18 22:27:57 +0500

    prepared

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-
 const authApi = require('./routes/auth');
 const DbConnect = require('./config/config');
 const postApi = require('./routes/posts');
@@ -11,20 +10,20 @@ const app = express();
 
 DbConnect();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(__dirname, 'client', 'build', 'index.html');
-  });
-}
-
 app.use(express.json());
 
 app.use('/api/profile', profileApi);
 app.use('/api/auth', authApi);
 app.use('/api/user', userApi);
 app.use('/api/post', postApi);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
